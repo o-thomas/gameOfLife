2b6be3b21eaf6bdbab791a8aac7c3cee99159381
o-thomas <oliver-thomas@live.fr>
2020-07-18 23:31:53 +0200

    the generation of grids and cells works + generation of my gridComponent

--- src/app/class/Grid.ts
+++ src/app/class/Grid.ts
@@ -1,17 +1,12 @@
-export class Grid{
-  id: number;
+import { Cell } from './Cell';
+import { Coordinate } from './Coordinate';
+export class Grid {
   axeYLength: number;
   axeXLength: number;
-  constructor(id: number, axeYLength: number, axeXLength: number) {
-    this.id = id;
+  cellList: Array <Cell>;
+  constructor( axeYLength: number, axeXLength: number) {
     this.axeYLength = axeYLength;
     this.axeXLength = axeXLength;
-  }
-  getId(): number{
-    return this.id;
-  }
-  setId(id): void{
-    this.id = id;
   }
   getAxeYLength(): number{
     return this.axeYLength;
@@ -25,5 +20,47 @@ export class Grid{
   setAxeXLength(axeXLength): void{
     this.axeXLength = axeXLength;
   }
+  getCellList(): any{
+    return this.cellList;
+  }
+  setCellList(cellList): void{
+    this.cellList = cellList;
+  }
+  createCells(): void {
+    let cellList = [];
+    console.log(this.axeYLength * this.axeXLength);
+    for (let i = 0; i < this.axeYLength; ++i) {
+        for (let j = 0; j < this.axeXLength; ++j) {
+          let coordinate = new Coordinate(j, i);
+          let cell = new Cell(coordinate, false);
+          cellList.push(cell);
+          this.cellList = cellList;
+        }
+      }
+  }
 
+  detectAdjacentCell(): void {
+    for (let cell of this.cellList) {
+      let listCell: Array <Cell> = [];
+      let coordinatesList: Array<Coordinate> = [];
+      let x = cell.coordinate.x;
+      let y = cell.coordinate.y;
+      coordinatesList[0] = new Coordinate(x + 1, y);
+      coordinatesList[1] = new Coordinate(x - 1, y);
+      coordinatesList[2]  = new Coordinate(x, y + 1);
+      coordinatesList[3] = new Coordinate(x, y - 1);
+      coordinatesList[4] = new Coordinate(x + 1 , y + 1);
+      coordinatesList[5] = new Coordinate(x + 1 , y - 1);
+      coordinatesList[6] = new Coordinate(x - 1 , y + 1);
+      coordinatesList[7] = new Coordinate(x - 1 , y - 1);
+      for (let target of this.cellList){
+        for (let coordinate of coordinatesList){
+          if (target.coordinate.getX() === coordinate.x && target.coordinate.getY() === coordinate.y){
+            listCell.push(target);
+          }
+        }
+      }
+      cell.setAdjacentCells(listCell);
+    }
+  }
 }
