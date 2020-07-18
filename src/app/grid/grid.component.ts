20033a53f6d59cf87f49523034b42f2c8ac171fb
o-thomas <oliver-thomas@live.fr>
2020-07-18 23:44:49 +0200

    the generation of grids and cells works + generation of my gridComponent

---
+++ src/app/grid/grid.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Grid} from '../class/Grid';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
